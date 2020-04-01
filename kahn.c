924e39d4d55d55f16b96c7ba1be1b46c36987a7b
Manos Kampourakis <ekampourakis@ceid.upatras.gr>
2020-04-01 14:11:14 +0300

    Create queue structure

--- kahn.c
+++ kahn.c
@@ -1,7 +1,5 @@
 #include "kahn.h"
 
-#include <stdio.h>
-
 int main(int argc, const char *argv[]) {
 	printf("Hello Kahn\n");
 }
---
+++ kahn.h
@@ -0,0 +1,8 @@
+#ifndef KAHN_KAHN_H
+#define KAHN_KAHN_H
+
+#include <stdio.h>
+
+#include "queue.h"
+
+#endif
