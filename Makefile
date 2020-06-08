70a2de09350c8310d3e32f12d474e2b69100c71e
Manos Kampourakis <ekampourakis@ceid.upatras.gr>
2020-06-08 14:08:05 +0300

    Create vector implementation

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=gcc
 CFLAGS=-fopenmp
 OBJ=kahn
-FILES=graph.c mmio.c
+FILES=vector.c graph.c mmio.c
 
 
 # Compile without optimizations
---
+++ vector.h
@@ -0,0 +1,21 @@
+#ifndef KAHN_VECTOR_H
+#define KAHN_VECTOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct vector_ {
+	void **data;
+	int size;
+	int count;
+} vector;
+
+void vector_init(vector *);
+int vector_count(vector *);
+void vector_add(vector *, void *);
+void vector_set(vector *, int, void *);
+void *vector_get(vector *, int);
+void vector_delete(vector *, int);
+void vector_free(vector *);
+
+#endif
